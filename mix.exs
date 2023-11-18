d9304ff048600c2baee73d7def615a2825847526
Matthew Whitworth <matt@tallarium.com>
2023-11-18 10:00:39 +0000

    floki supports 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Monocle.MixProject do
     [
       app: :monocle,
       version: "0.0.1",
-      elixir: "~> 1.15",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
